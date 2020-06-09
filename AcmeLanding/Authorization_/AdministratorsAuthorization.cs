74c7009bddc5505e554fcf249bcc1b7474c4ea2b
Daniella Clemmensen <dani7135@edu.ucl.dk>
2020-06-09 19:55:25 +0200

    Add so it's ready to roles

---
+++ AcmeLanding/Authorization_/AdministratorsAuthorization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace AcmeLanding.Authorization_.Authorization
+{
+    public class AdministratorsAuthorization:AuthorizationHandler<OperationAuthorizationRequirement, Submission_Model>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, Submission_Model resource)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+           /* if (context.User.IsInRole(Constants.ContactAdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }*/
+            return Task.CompletedTask;
+        }
+    }
+}
